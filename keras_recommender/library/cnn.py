fabc02373d732ff46c5863214ff76b8d385b83c8
xschen <xs0040@gmail.com>
2017-12-31 14:30:36 +0800

    complete the implementation of the content-based filtering using CNN

--- keras_recommender/library/cnn.py
+++ keras_recommender/library/cnn.py
@@ -5,12 +5,31 @@ import os
 
 class Vgg16ContentBaseFiltering(object):
 
+    model_name = 'vgg16-cbf'
+
     def __init__(self):
         self.matrix_res = None
         self.similarity_deep = None
         self.model = VGG16(include_top=False, weights='imagenet')
         self.matrix_idx_to_item_id = None
         self.item_id_to_matrix_idx = None
+
+    @staticmethod
+    def get_config_file_path(model_dir_path):
+        predictions_file = model_dir_path + '/' + Vgg16ContentBaseFiltering.model_name + '-matrix_res.npz'
+        return predictions_file
+
+    @staticmethod
+    def get_weight_file_path(model_dir_path):
+        return model_dir_path + '/' + Vgg16ContentBaseFiltering.model_name + '-similarity-matrix.npy'
+
+    def load_model(self, config_file_path, weight_file_path):
+        data = np.load(Vgg16ContentBaseFiltering.get_weight_file_path(model_dir_path=config_file_path))
+        self.matrix_res = data['matrix_res']
+        self.item_id_to_matrix_idx = data['id2idx']
+        self.matrix_idx_to_item_id = data['idx2id']
+
+        self.similarity_deep = np.load(weight_file_path)['similarity_deep']
 
     def fit(self, item_id2img, model_dir_path=None):
         if model_dir_path is None:
@@ -25,20 +44,20 @@ class Vgg16ContentBaseFiltering(object):
         num_items = len(item_id2img.keys())
         print('Number of items = {}'.format(num_items))
         predictions_file = model_dir_path + '/matrix_res.npz'
-        if not os.path.exists(predictions_file):
-            matrix_res = np.zeros([num_items, 25088])
-            for i, (item_id, item_img) in enumerate(item_id2img.items()):
-                print('Predicting for item = {}'.format(item_id))
-                matrix_res[i, :] = self.model.predict(item_img).ravel()
-            np.savez_compressed(file=predictions_file, matrix_res=matrix_res)
-        else:
-            matrix_res = np.load(predictions_file)['matrix_res']
 
-        similarity_deep = matrix_res.dot(matrix_res.T)
+        self.matrix_res = np.zeros([num_items, 25088])
+        for i, (item_id, item_img) in enumerate(item_id2img.items()):
+            print('Predicting for item = {}'.format(item_id))
+            self.matrix_res[i, :] = self.model.predict(item_img).ravel()
+        np.savez_compressed(file=predictions_file, matrix_res=self.matrix_res, idx2id=self.matrix_idx_to_item_id, id2idx=self.item_id_to_matrix_idx)
+
+        similarity_deep = self.matrix_res.dot(self.matrix_res.T)
         norms = np.array([np.sqrt(np.diagonal(similarity_deep))])
         similarity_deep = similarity_deep / (norms * norms.T)
         print(similarity_deep.shape)
         self.similarity_deep = similarity_deep
+        np.savez_compressed(file=Vgg16ContentBaseFiltering.get_weight_file_path(model_dir_path),
+                            similarity_deep=self.similarity_deep)
 
     def recommend_closest_items(self, target_item_id, top_k=None):
         if top_k is None:
--- keras_recommender/training/vgg16_content_based_filtering.py
+++ keras_recommender/training/vgg16_content_based_filtering.py
@@ -2,9 +2,15 @@ import pandas as pd
 import numpy as np
 from keras.applications.vgg16 import preprocess_input
 from keras.preprocessing import image as kimage
+from keras_recommender.library.cnn import Vgg16ContentBaseFiltering
+import os
+import pickle
+from glob import glob
 
 def main():
     data_dir_path = './data/ml-latest-small'
+    poster_dir_path = './data/posters'
+    output_dir_path = './data/models'
 
     np.set_printoptions(threshold=np.nan)
     pd.set_option('display.height', 1000)
@@ -18,6 +24,24 @@ def main():
     df = pd.merge(pd.merge(df, df_id, on='movieId'), df_movie_names, on='movieId')
 
     print(df.head())
+
+    data_file = data_dir_path + '/imdb_id_to_image_dict.data'
+    if not os.path.exists(data_file):
+        imdb_id_to_image_dict = dict()
+        for poster_file in glob(poster_dir_path + '/*.jpg'):  # debug here
+            print('Loading img at {}'.format(poster_file))
+            img = kimage.load_img(poster_file, target_size=(224, 224))
+            img = preprocess_input(np.expand_dims(kimage.img_to_array(img), axis=0))
+            imdb_id = poster_file.split('/')[-1].split('.')[0]
+            imdb_id_to_image_dict[imdb_id] = img
+        pickle.dump(file=open(data_file, 'wb'), obj=imdb_id_to_image_dict)
+    else:
+        imdb_id_to_image_dict = pickle.load(file=open(data_file, 'rb'))
+
+    recommender = Vgg16ContentBaseFiltering()
+    recommender.fit(imdb_id_to_image_dict, model_dir_path=output_dir_path)
+
+
 
 
 if __name__ == '__main__':
